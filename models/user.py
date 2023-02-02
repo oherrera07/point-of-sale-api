02057701c97cbc6f837ad8a6bc1cf2cf44adffd8
Omar <omarherrera_07@icloud.com>
2023-02-02 15:01:05 -0600

    Created User Table Using Migrations

--- models/user.py
+++ models/user.py
@@ -5,3 +5,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     username = db.Column(db.String(16), unique=True, nullable=False)
     password = db.Column(db.String(64), nullable=False)
+    salt = db.Column(db.String(64), nullable=False)
+    created_at = db.Column(db.TIMESTAMP, nullable=False)
+    updated_at = db.Column(db.TIMESTAMP, nullable=False)
+    deleted_at = db.Column(db.TIMESTAMP, nullable=False)
